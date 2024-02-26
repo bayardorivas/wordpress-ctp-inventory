3135f07502ba00257d4adbe2553b946b891192d8
Bayardo Rivas <bayardo.rivas@gmail.com>
2024-02-25 18:42:30 -0600

    Setup connection to DB and Add new endpoints to list catalogs cores, themes, plugins and sites.

--- src/index.cjs
+++ src/index.cjs
@@ -1,26 +1,77 @@
 const Express = require('express');
+const db = require("../db/connection.cjs")
 
 const app = Express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+
+app.get("/api", (req, res) => {
   res.send("WordPress-CTP Inventory API!");
 });
 
-app.get("/sites", (req, res) => {
-  res.send("sites zone");
+app.get("/api/sites", (req, res) => {
+  //  Send all sites in Database
+  const sql = "SELECT * from sites";
+  let params = [];
+  db.all(sql,params, (err,rows) => {
+    if (err) {
+      res.status(400).json({"error": err.message})
+      return;
+    } 
+    res.json({
+      "message": "API hit Sites",
+      "data": rows
+    })
+  })
 });
 
-app.get("/cores", (req, res) => {
-  res.send("core zone");
+app.get("/api/cores", (req, res) => {
+  // List of Cores installed
+  const sql = "SELECT * from cores";
+  let params = [];
+  db.all(sql,params, (err,rows) => {
+    if (err) {
+      res.status(400).json({"error": err.message})
+      return;
+    } 
+    res.json({
+      "message": "API hit Cores",
+      "data": rows
+    })
+  })
+
 });
 
-app.get("/themes", (req, res) => {
-  res.send("themes zone");
+app.get("/api/themes", (req, res) => {
+  // List of Themes installed
+  const sql = "SELECT * from themes";
+  let params = [];
+  db.all(sql,params, (err,rows) => {
+    if (err) {
+      res.status(400).json({"error": err.message})
+      return;
+    } 
+    res.json({
+      "message": "API hit Themes",
+      "data": rows
+    })
+  })
 });
 
-app.get("/plugins", (req, res) => {
-  res.send("plugins zone");
+app.get("/api/plugins", (req, res) => {
+  // List of Plugins installed
+  const sql = "SELECT * from plugins";
+  let params = [];
+  db.all(sql,params, (err,rows) => {
+    if (err) {
+      res.status(400).json({"error": err.message})
+      return;
+    } 
+    res.json({
+      "message": "API hit Plugins",
+      "data": rows
+    })
+  })
 });
 
 // Default response for any other request
@@ -30,5 +81,5 @@ app.use(function(req, res){
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port http://localhost:${port}`);
 });
